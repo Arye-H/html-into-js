be30f5a97ed3b4fa2a713cd7f19ed574ed37afd1
Arye-H <84711174+Arye-H@users.noreply.github.com>
2022-01-01 21:54:04 +0200

    minor changes

--- ExampleProject/scripts/main.js
+++ ExampleProject/scripts/main.js
@@ -3,15 +3,16 @@ function loadFunction() {
     getL("MainDiv").style.backgroundColor = "lightpink";
 
     buildL("MainDiv", "h1", { id: "MainHeader" }, "HTML Into JS Example");
-    buildL("MainDiv", "div", { id: "DivDate", style: "padding: 10px; border: 1px solid black;" });
-    buildL("DivDate", "button", { id: "BtnGetDate", onclick: () => { getDate(); } }, "Get a current date and time");
+    buildL("MainDiv", "div", { id: "DivDate", style: "border: 1px solid black;" });
+    buildL("DivDate", "label", { for: "BtnGetDate", style: "display: block;" }, "Click the button to view the current date and time");
+    buildL("DivDate", "button", { id: "BtnGetDate", onclick: () => { getDate(); } }, "Click me!");
 
 };
 
 function getDate() {
 
     getL("DivDate").removeChildren();
-    
+
     buildL("DivDate", "h3", { id: "DateString" }, new Date().toLocaleString());
 
 };
